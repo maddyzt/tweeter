90b04e3cbf197180b3206ecb0c013bdd3338ba28
Maddy Tan <maddytan7@gmail.com>
2022-05-26 18:34:41 +0000

    includes updated dependencies in package.json

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,39 +6,39 @@
 
 $(document).ready(function() {
 
-// show/hide the form when the arrow is clicked in the nav
-$('.new-tweet').hide();
-$('.down-arrow').click(function() {
-  $('.new-tweet').slideToggle();
-  $('.tweet-input').focus();
-})
+  // show/hide the form when the arrow is clicked in the nav
+  $('.new-tweet').hide();
+  $('.down-arrow').click(function() {
+    $('.new-tweet').slideToggle();
+    $('.tweet-input').focus();
+  });
 
-// show scroll button upon scroll down
-let $btn = $('.scroll-btn');
-$btn.hide();
-$(window).scroll(function() {
-  if($(window).scrollTop() > 300) {
-    $btn.show();
-  } else {
-    $btn.hide();
-  }
-})
+  // show scroll button upon scroll down
+  let $btn = $('.scroll-btn');
+  $btn.hide();
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > 300) {
+      $btn.show();
+    } else {
+      $btn.hide();
+    }
+  });
 
-// scroll to top on click
-$('.scroll-btn').click(function() {
-  $('html,body').animate({ scrollTop: 0 }, 'fast');
-})
+  // scroll to top on click
+  $('.scroll-btn').click(function() {
+    $('html,body').animate({ scrollTop: 0 }, 'fast');
+  });
 
-// define escape function
-const escape = function (str) {
-  let div = document.createElement("div");
-  div.appendChild(document.createTextNode(str));
-  return div.innerHTML;
-};
+  // define escape function
+  const escape = function(str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
 
-// function to transform tweet object into html
-const createTweetElement = function(tweetObject) {
-  let tweet = `
+  // function to transform tweet object into html
+  const createTweetElement = function(tweetObject) {
+    let tweet = `
       <article class="tweet-container">
         <header class="tweet-header">
           <span class="tweet-user"><i id="smiley" class="fa-solid fa-face-grin-stars"></i>${escape(tweetObject.user.name)}</span>
@@ -54,58 +54,59 @@ const createTweetElement = function(tweetObject) {
           </span>
         </footer>
       </article>
-  `
-  return tweet;
-};
+  `;
+    return tweet;
+  };
 
-// function to loop through tweets and createTweetElement for each
-const renderTweets = function(tweetArray) {
-  tweetArray.forEach( tweet => {
-    let val = createTweetElement(tweet);
-    $('#tweet-list').prepend(val);
-  })
-};
+  // function to loop through tweets and createTweetElement for each
+  const renderTweets = function(tweetArray) {
+    tweetArray.forEach(tweet => {
+      let val = createTweetElement(tweet);
+      $('#tweet-list').prepend(val);
+    });
+  };
 
-// function to post tweets
-const postTweets = function () {
-  $('#tweet-form').submit(function(event) {
-  event.preventDefault();
-  const triangleAlert = `<i class="fa-solid fa-triangle-exclamation"></i>`
-  const errorMessage1 = `<span>${triangleAlert} Cannot post 0 characters ${triangleAlert}</span>`;
-  const errorMessage2 = `<span>${triangleAlert} Maximum character length exceeded ${triangleAlert}</span>`;
+  // function to post tweets
+  const postTweets = function() {
+    $('#tweet-form').submit(function(event) {
+      event.preventDefault();
+      const triangleAlert = `<i class="fa-solid fa-triangle-exclamation"></i>`;
+      const errorMessage1 = `<span>${triangleAlert} Cannot post 0 characters ${triangleAlert}</span>`;
+      const errorMessage2 = `<span>${triangleAlert} Maximum character length exceeded ${triangleAlert}</span>`;
   
-  $('.error-text').slideUp(400).text('');
+      $('.error-text').slideUp(400).text('');
 
-  if ($('.tweet-input').val().length > 140) {
-    return $('.error-text').html(errorMessage2).slideDown();
-  } else if ($('.tweet-input').val().length === 0) {
-    return $('.error-text').html(errorMessage1).slideDown();
-  }
+      if ($('.tweet-input').val().length > 140) {
+        return $('.error-text').html(errorMessage2).slideDown();
+      } else if ($('.tweet-input').val().length === 0) {
+        return $('.error-text').html(errorMessage1).slideDown();
+      }
 
-  $.ajax({
-    url: "/tweets",
-    method: "POST",
-    data: $(this).serialize()
-  }).then(response => {
-    $('#tweet-form').children('.tweet-input').val('');
-    $('.counter').val(140);
-    loadTweets();
-  }).catch(err => console.log(err));
-}
-)};
+      $.ajax({
+        url: "/tweets",
+        method: "POST",
+        data: $(this).serialize()
+      }).then(response => {
+        $('#tweet-form').children('.tweet-input').val('');
+        $('.counter').val(140);
+        loadTweets();
+      }).catch(err => console.log(err));
+    }
+    );
+  };
 
-// function to load tweets
-const loadTweets = function () {
-  $.ajax({
-    url: "/tweets",
-    method: "GET",
-  }).then(function(response) {
-    renderTweets(response);
-  })
-}
+  // function to load tweets
+  const loadTweets = function() {
+    $.ajax({
+      url: "/tweets",
+      method: "GET",
+    }).then(function(response) {
+      renderTweets(response);
+    });
+  };
 
-loadTweets();
-postTweets();
+  loadTweets();
+  postTweets();
 
 
 });
